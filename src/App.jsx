e4ad58b8d5c83b649133c2865f50781363ef79e2
loairebouh <aoue42@MacBook-Pro-de-LOAI-rebouh.local>
2024-09-04 09:40:52 +0100

    deal with se effect by making a digital clock

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import DigitalClock from "./DigitalClock";
+
+function App() {
+	return (
+		<>
+			<div>
+				<DigitalClock></DigitalClock>
+			</div>
+		</>
+	);
+}
+
+export default App;
